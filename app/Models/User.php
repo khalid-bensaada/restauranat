4e3919efd3fc470864fa8f1bd121c3e5b88310fc
khalid-bensaada <kbensaada953@gmail.com>
2026-02-05 12:00:43 +0100

    added model of role

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'role',
+        'role_id',
     ];
 
     /**
@@ -45,5 +45,9 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
     }
 }
