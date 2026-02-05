d755c2497b6a38428286df8c2e69743336bda383
khalid-bensaada <kbensaada953@gmail.com>
2026-02-05 12:01:23 +0100

    added a table of roles and of user

--- database/migrations/2026_02_04_153912_create_roles_table.php
+++ database/migrations/2026_02_04_153912_create_roles_table.php
@@ -6,23 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        Schema::table('restaurants', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id(); 
+            $table->string('name')->unique();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::table('restaurants', function (Blueprint $table) {
-            //
-        });
+        Schema::dropIfExists('roles');
     }
 };
