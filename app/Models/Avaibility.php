0f7f884de3ba19c6b3aff299e4ad110c5d8cf089
khalid-bensaada <kbensaada953@gmail.com>
2026-02-11 17:26:54 +0100

    added model of availability

--- app/Models/Avaibility.php
+++ app/Models/Avaibility.php
@@ -4,11 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Availability extends Model
+class Avaibility extends Model
 {
     protected $fillable = [
+        'restaurant_id',
+        'date',
         'start_time',
         'capacity',
-        'booked_count',
     ];
 }
